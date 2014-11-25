6fdd27a329fae6a7e381bc304bf4458ba6e5299c
Rueben Ramirez <ruebenramirez@gmail.com>
2014-11-25 09:23:22 -0600

    better palm detection setting

--- bin/configure-mouse-palm-detection.sh
+++ bin/configure-mouse-palm-detection.sh
@@ -4,5 +4,5 @@
 # https://wiki.archlinux.org/index.php/Touchpad_Synaptics#Using_automatic_palm_detection
 
 synclient PalmDetect=1
-synclient PalmMinWidth=10
+synclient PalmMinWidth=12
 synclient PalmMinZ=200
