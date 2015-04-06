31e65ce21a8cd5d28acacc95b378a175a983b498
Rueben Ramirez <ruebenramirez@gmail.com>
2015-04-06 17:49:16 -0500

    adding vpn to statusbar

--- .xsetroot.sh
+++ .xsetroot.sh
@@ -5,5 +5,8 @@ BAT="Bat. $(acpi -b | awk '{ print $4 " " $5 }' | tr -d ',')"
 VOL="vol. $(sh /home/rramirez/bin/volume-get.sh)"
 BRIGHT="bLight. $(light -G | cut -d . -f 1)"
 LOCALTIME=$(date +%m/%d\/%Y\ %r)
+VPN="$(test $(ps aux | grep -i 'openconnect.*rackspace' | wc -l) -gt 1 && echo '<< VPN-Rackspace ')"
+VPN="$VPN$(test $(ps aux | grep -i 'vpnc nt.conf' | wc -l) -gt 1 && echo '<< VPN-NewTek ')"
 
-xsetroot -name "$BAT << $VOL << $BRIGHT << $LOCALTIME"
+
+xsetroot -name "$BAT << $VOL << $BRIGHT $VPN<< $LOCALTIME"
