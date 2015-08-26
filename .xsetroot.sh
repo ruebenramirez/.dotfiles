763c6f08fda885a61914537e41fc503c33f1c554
Rueben Ramirez <ruebenramirez@gmail.com>
2015-08-26 17:37:07 -0500

    handle acpi multiple listings where some are unavailable

--- .xsetroot.sh
+++ .xsetroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set +e
 
-BAT="Bat. $(acpi -b | awk '{ print $4 " " $5 }' | tr -d ',')"
+BAT="Bat. $(acpi -b | grep -v unavailable | awk '{ print $4 " " $5 }' | tr -d ',')"
 VOL="$(sh /home/rramirez/bin/volume-get.sh)"
 BRIGHT="bLight. $(light -G | cut -d . -f 1)"
 LOCALTIME=$(date +%m/%d\/%Y\ %r)
