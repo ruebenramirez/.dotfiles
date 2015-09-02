7a2b538a453be263741fc3aeee56c7467bf0fc05
Rueben Ramirez <ruebenramirez@gmail.com>
2015-09-02 17:12:43 -0500

    update vpn notifier in DWM menubar

--- .xsetroot.sh
+++ .xsetroot.sh
@@ -5,8 +5,6 @@ BAT="Bat. $(acpi -b | grep -v unavailable | awk '{ print $4 " " $5 }' | tr -d ',
 VOL="$(sh /home/rramirez/bin/volume-get.sh)"
 BRIGHT="bLight. $(light -G | cut -d . -f 1)"
 LOCALTIME=$(date +%m/%d\/%Y\ %r)
-VPN="$(test $(ps aux | grep -i 'openconnect.*rackspace' | wc -l) -gt 1 && echo '<< VPN-Rackspace ')"
-VPN="$VPN$(test $(ps aux | grep -i 'vpnc nt.conf' | wc -l) -gt 1 && echo '<< VPN-NewTek ')"
-
+VPN="$VPN$(test $(pgrep 'forticlient' | wc -l) -gt 1 && echo '<< VPN-NewTek ')"
 
 xsetroot -name "$BAT << $VOL << $BRIGHT $VPN<< $LOCALTIME"
