e297b8ae86c969c2eff19ed404f43ac068e8f147
Rueben Ramirez <ruebenramirez@gmail.com>
2015-10-26 16:55:31 -0500

    display wifi in status dwm bar

--- .xsetroot.sh
+++ .xsetroot.sh
@@ -4,7 +4,8 @@ set +e
 BAT="Bat. $(acpi -b | grep -v unavailable | awk '{ print $4 " " $5 }' | tr -d ',')"
 VOL="$(sh /home/rramirez/bin/volume-get.sh)"
 BRIGHT="bLight. $(light -G | cut -d . -f 1)"
-LOCALTIME=$(date +%m/%d\/%Y\ %r)
+WIFI="wifi: $(nmcli -t -f active,ssid dev wifi | grep ^yes | cut -d ':' -f 2)"
 VPN="$VPN$(test $(pgrep 'forticlient' | wc -l) -gt 1 && echo '<< VPN-NewTek ')"
+LOCALTIME=$(date +%m/%d\/%Y\ %r)
 
-xsetroot -name "$BAT << $VOL << $BRIGHT $VPN<< $LOCALTIME"
+xsetroot -name "$BAT << $VOL << $WIFI << $BRIGHT $VPN<< $LOCALTIME"
