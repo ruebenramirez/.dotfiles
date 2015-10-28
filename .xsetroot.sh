5f2110320bb36ddfa1bebd616e9f6e73e111c3d8
Rueben Ramirez <ruebenramirez@gmail.com>
2015-10-28 13:55:53 -0500

    simplify the cpu temp grab

--- .xsetroot.sh
+++ .xsetroot.sh
@@ -6,7 +6,7 @@ VOL="$(sh /home/rramirez/bin/volume-get.sh)"
 BRIGHT="bLight. $(light -G | cut -d . -f 1)"
 WIFI="wifi: $(nmcli -t -f active,ssid dev wifi | grep ^yes | cut -d ':' -f 2)"
 VPN="$VPN$(test $(pgrep 'forticlient' | wc -l) -gt 1 && echo '<< VPN-NewTek ')"
-CPU_TEMP="CPU temp: $(sensors | grep "Physical id 0" | cut -d ':' -f2 | awk '{ print $1 }')"
+CPU_TEMP="CPU temp: $(sensors | grep "Physical id 0" | awk '{ print $4 }')"
 LOCALTIME=$(date +%m/%d\/%Y\ %r)
 
 xsetroot -name "$BAT << $VOL << $WIFI << $BRIGHT $VPN<< $CPU_TEMP << $LOCALTIME"
