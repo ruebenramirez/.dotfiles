970d4b967d846535acb2f7af816d5d676a77223a
Rueben Ramirez <ruebenramirez@gmail.com>
2015-10-28 13:51:11 -0500

    adding cpu temp to dwm status

--- .xsetroot.sh
+++ .xsetroot.sh
@@ -6,6 +6,7 @@ VOL="$(sh /home/rramirez/bin/volume-get.sh)"
 BRIGHT="bLight. $(light -G | cut -d . -f 1)"
 WIFI="wifi: $(nmcli -t -f active,ssid dev wifi | grep ^yes | cut -d ':' -f 2)"
 VPN="$VPN$(test $(pgrep 'forticlient' | wc -l) -gt 1 && echo '<< VPN-NewTek ')"
+CPU_TEMP="CPU temp: $(sensors | grep "Physical id 0" | cut -d ':' -f2 | awk '{ print $1 }')"
 LOCALTIME=$(date +%m/%d\/%Y\ %r)
 
-xsetroot -name "$BAT << $VOL << $WIFI << $BRIGHT $VPN<< $LOCALTIME"
+xsetroot -name "$BAT << $VOL << $WIFI << $BRIGHT $VPN<< $CPU_TEMP << $LOCALTIME"
