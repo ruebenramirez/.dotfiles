5d83bb93357894acb7d439e2ad4341fe9c88a972
Rueben Ramirez <ruebenramirez@gmail.com>
2015-12-26 09:45:16 -0600

    cleanup init display

--- bin/init-display.sh
+++ bin/init-display.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-if [[ $(hostname -f) = "lefrank" ]]
-then
-    # Personal MacBookPro
+hostname=`hostname -f`
+logger '~/bin/init-display'
+
+if [ $hostname = 'ThinkPad' ]; then
+    logger 'setting up display for newtek work thinkpad'
     xrandr --auto
-    xrandr --output DP-0 --left-of LVDS-0
-    xrandr --output DP-1 --left-of LVDS-0
-elif [[ $(hostname -f) = "rramirez-dev-vm" ]]
-then
-    # Dev VM 
-    xrandr --auto
-    xrandr --output Virtual1 --mode 1360x768
-elif [[ $(hostname -f) = "minfrank" ]]
-then
-    # Work Machine
+    xrandr --output LVDS-1 --size 1600x900
+    xrandr --output DP-1 --left-of LVDS-1
+elif [ $(hostname -f) = "minfrank" ]; then
+    logger 'setting up display for asus ux-305a'
     xrandr --auto
     xrandr --output HDMI1 --mode 1920x1200 --left-of eDP1
 else
-    Logger "~/bin/init-display: not sure what monitor config to use"
+    logger "not sure what monitor config to use"
 fi
