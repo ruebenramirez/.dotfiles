2553e3238a0a2f1a7226e31468d49557725e724c
Rueben Ramirez <ruebenramirez@gmail.com>
2016-01-11 17:38:49 -0600

    updating ports for work display rescreen

--- bin/init-display.sh
+++ bin/init-display.sh
@@ -6,8 +6,8 @@ logger '~/bin/init-display'
 if [ $hostname = 'ThinkPad' ]; then
     logger 'setting up display for newtek work thinkpad'
     xrandr --auto
-    xrandr --output LVDS-1 --size 1600x900
-    xrandr --output DP-1 --left-of LVDS-1
+    xrandr --output LVDS-0 --size 1600x900
+    xrandr --output DP-3 --left-of LVDS-0
 elif [ $(hostname -f) = "minfrank" ]; then
     logger 'setting up display for asus ux-305a'
     xrandr --auto
