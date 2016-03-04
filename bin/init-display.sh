15aa5e69c3a6c4cee9af280d08a220e516857623
Rueben Ramirez <ruebenramirez@gmail.com>
2016-03-04 17:56:06 -0600

    add mac screen setup

--- bin/init-display.sh
+++ bin/init-display.sh
@@ -12,6 +12,14 @@ elif [ $(hostname -f) = "minfrank" ]; then
     logger 'setting up display for asus ux-305a'
     xrandr --auto
     xrandr --output HDMI1 --mode 1920x1200 --left-of eDP1
+elif [ $(hostname -f) = "frankenmac" ]; then
+    logger 'setting up display for early 2015 macbook pro 13'
+    xrandr --auto
+    xrandr --output eDP1 --mode 1920x1200
+    xrandr --output DP1 --left-of eDP1
+    xrandr --output DP2 --left-of eDP1
+    xrandr --output HDMI1 --left-of eDP1
+    xrandr --output HDMI2 --left-of eDP1
 else
     logger "not sure what monitor config to use"
 fi
