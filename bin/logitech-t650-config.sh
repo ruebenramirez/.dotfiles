09bd8c698db7e5066e7bceb2fbd108febbc43333
Rueben Ramirez <ruebenramirez@gmail.com>
2016-06-14 19:12:21 -0500

    natural scrolling for logitech t650 trackpad

--- bin/logitech-t650-config.sh
+++ bin/logitech-t650-config.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 # natural scrolling (inverted scrolling like apple devices)
-xinput set-button-map "Logitech Unifying Device. Wireless PID:4101" 1 3 1 5 4 7 6
-
-
-# 3 finger press right click (disable bottom right trackpad for right click)
-xinput set-button-map "Logitech Unifying Device. Wireless PID:4101" 1 3 1
+xinput set-button-map "Logitech Rechargeable Touchpad T650" 1 2 3 5 4 7 6 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
