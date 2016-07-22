e61604444593f88b789b5d19b3e10c79922660d4
Rueben Ramirez <ruebenramirez@gmail.com>
2016-07-22 17:47:21 -0500

    place external monitor above laptop panel
    When I was using DWM, there wasn't a nice way to switch physical
    screens without xdotool (and there wasn't a nice way to change 'y'
    coordinate location of mouse...now that I'm in i3wm I don't have this
    'y' coordinate problem!

--- bin/init-display.sh
+++ bin/init-display.sh
@@ -9,26 +9,27 @@ xrandr --auto
 if [ $hostname = 'ThinkPad' ]; then
     echo 'setting up display for newtek w520';
     xrandr --output LVDS2 --mode 1600x900
-    xrandr --output DP-1-1 --left-of LVDS2
+    xrandr --output DP-1-1 --above LVDS2
 elif [ $hostname = 'my-ThinkPad' ]; then
     echo 'setting up display for personal w520'
     xrandr --output LVDS-0 --mode 1920x1080
-    xrandr --output DisplayPort-0 --left-of LVDS-0
-    xrandr --output DisplayPort-2 --left-of LVDS-0
+    xrandr --output DisplayPort-0 --above LVDS-0
+    xrandr --output DisplayPort-2 --above LVDS-0
 elif [ $hostname = 'ThinkPad-X220' ]; then
     echo 'setting up display for x220'
     xrandr --output LVDS1 --mode 1366x768
-    xrandr --output DP1 --auto --left-of LVDS1
-    xrandr --output HDMI1 --auto --left-of LVDS1
-    xrandr --output VGA1 --auto --left-of LVDS1
+    xrandr --output DP1 --auto --above LVDS1
+    xrandr --output DP2 --auto --above LVDS1
+    xrandr --output HDMI1 --auto --above LVDS1
+    xrandr --output VGA1 --auto --above LVDS1
 elif [ $hostname = "frankenmac" ]; then
     echo 'setting up display for (12,1) macbook pro 13'
     xrandr --output eDP1 --mode 1920x1200
-    xrandr --output DP1 --left-of eDP1 --mode 2560x1440
-    xrandr --output DP1 --left-of eDP1 --mode 3440x1440
-    xrandr --output DP2 --left-of eDP1
-    xrandr --output HDMI1 --left-of eDP1
-    xrandr --output HDMI2 --left-of eDP1
+    xrandr --output DP1 --above eDP1 --mode 2560x1440
+    xrandr --output DP1 --above eDP1 --mode 3440x1440
+    xrandr --output DP2 --above eDP1
+    xrandr --output HDMI1 --above eDP1
+    xrandr --output HDMI2 --above eDP1
 else
     echo 'not sure what monitor config to use';
 fi
