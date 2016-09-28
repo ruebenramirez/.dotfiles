90bb6ca2ece348b83e28b6f718a2ca079489b789
Rueben Ramirez <ruebenramirez@gmail.com>
2016-09-27 21:04:28 -0500

    reset chrome for clean start

--- bin/killchrome.sh
+++ bin/killchrome.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+google-chrome&
+google-chrome&
+google-chrome&
+google-chrome&
+google-chrome&
+google-chrome&
+google-chrome&
+
 while [ $(ps aux | grep chrome | egrep -v 'killchrome|grep' | wc -l) -gt 0 ]; do
     killall chrome;
 done
