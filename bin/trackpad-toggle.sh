2a050f764acaf430b69dbe8d17e85187288e8ef2
Rueben Ramirez <ruebenramirez@gmail.com>
2017-08-19 15:03:04 -0500

    trackpad toggle cleanup
    * had different scripts for different machines
    * now using a single script based on hostname (similar to rescreen logic)

--- bin/trackpad-toggle.sh
+++ bin/trackpad-toggle.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-synclient TouchpadOff=$(synclient -l | grep -c 'TouchpadOff.*=.*0')
+
+
+hostname=`hostname -f`
+if [ ! $hostname = 'XPS-15-9550' ]; then
+    synclient TouchpadOff=$(synclient -l | grep -c 'TouchpadOff.*=.*0')
+
+else
+    if [[ $(xinput --list "DLL06E4:01 06CB:7A13 Touchpad" | grep -i disabled | wc -l) > 0 ]]; then
+        xinput --enable "DLL06E4:01 06CB:7A13 Touchpad"
+        killall imwheel
+    else
+        xinput --disable "DLL06E4:01 06CB:7A13 Touchpad"
+        imwheel &
+    fi;
+fi;
