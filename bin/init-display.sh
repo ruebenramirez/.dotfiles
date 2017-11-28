261ec579bb59ffc3c131659d8c72bd682812b846
Rueben Ramirez <ruebenramirez@gmail.com>
2017-11-27 19:10:37 -0600

    update t420 screen config

--- bin/init-display.sh
+++ bin/init-display.sh
@@ -4,7 +4,7 @@ hostname=$(hostname -f)
 echo '~/bin/init-display'
 echo $hostname
 
-HOME_MONITOR=$(xrandr | egrep -i 'DP.*connected' | grep -v 'eDP' | wc -l)
+HOME_MONITOR=$(xrandr | egrep -i 'DP.*connected' | egrep -v 'disconnected|eDP' | wc -l)
 
 xrandr --auto
 if [[ $hostname == 'chromebook' ]]; then
@@ -50,16 +50,20 @@ elif [[ $hostname == 'ThinkPad-W520' ]]; then
     xrandr --output DP-1-2 --above LVDS2
     xrandr --output DP-1-3 --above LVDS1
     xrandr --output DP-1-3 --above LVDS2
-elif [[ $hostname == 'thinkpad-t420' ]]; then
-    echo 'setting up display for personal T420'
-    xrandr --output LVDS-0 --mode 1600x900
-    xrandr --output DisplayPort-0 --above LVDS-0
-    xrandr --output DisplayPort-2 --above LVDS-0
-    xrandr --output LVDS1 --mode 1920x1080
-    xrandr --output DP-1-2 --above LVDS1
-    xrandr --output DP-1-2 --above LVDS2
-    xrandr --output DP-1-3 --above LVDS1
-    xrandr --output DP-1-3 --above LVDS2
+elif [[ $hostname == 'le-laptop' ]]; then
+    if [[ $HOME_MONITOR == 1 ]]; then
+        echo "T420 external monitor found"
+        sh ~/.screenlayout/home.sh
+    else
+        echo 'setting up display for personal T420'
+        xrandr --output LVDS-1 --mode 1600x900
+        xrandr --output DP-1 --above LVDS-1
+        xrandr --output DP-2 --above LVDS-1
+        xrandr --output DP-3 --above LVDS-1
+        xrandr --output HDMI-1 --above LVDS-1
+        xrandr --output HDMI-2 --above LVDS-1
+        xrandr --output VGA-1 --above LVDS-1
+    fi
 elif [[ $hostname == "frankenmac" ]]; then
     echo 'setting up display for (12,1) macbook pro 13'
     xrandr --output eDP1 --mode 1920x1200
