cac1d810edde23e135fb36b9b249be96ef24bc20
Rueben Ramirez <ruebenramirez@gmail.com>
2020-05-18 09:43:33 -0500

    apple keyboard setup: swap esc/capslock

--- bin/setup-apple-keyboard.py
+++ bin/setup-apple-keyboard.py
@@ -16,6 +16,7 @@ def configure_apple_keyboard():
               echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode;
               echo 0 | sudo tee /sys/module/hid_apple/parameters/iso_layout;
               echo 1 | sudo tee /sys/module/hid_apple/parameters/swap_opt_cmd;
+              setxkbmap -option caps:swapescape
               """)
 
 
