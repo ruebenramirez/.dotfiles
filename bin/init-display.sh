647e24b5af30e3efffef2c8b7104f58a6edc9fb8
Rueben Ramirez <ruebenramirez@gmail.com>
2021-03-12 09:30:12 -0600

    external monitor setup and default brightness set programatically

--- bin/init-display.sh
+++ bin/init-display.sh
@@ -9,14 +9,19 @@ HOME_MONITOR=$(xrandr | egrep -i 'DP.*connected' | egrep -v 'disconnected|eDP' |
 xrandr --auto
 if [[ $HOME_MONITOR == 1 ]]; then
     echo "FOUND external display"
-    sh ~/.screenlayout/home.sh
+    sh ~/.screenlayout/default.sh
+
+    # max out external monitor screen brightness
+    sudo ddcutil setvcp 0x10 75
 else
     echo "no external display"
     xrandr \
         --dpi 96 \
-        --output eDP1 --mode 1600x900 --rotate normal \
+        --output eDP1 --mode 1920x1080 --rotate normal \
         --output DP1 --off \
         --output DP2 --off \
         --output HDMI2 --off \
         --output HDMI1 --off
 fi
+
+
