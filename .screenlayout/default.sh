8b0aae6da5fd7308cccdc3af092a17c8091ac979
Rueben Ramirez <ruebenramirez@gmail.com>
2021-04-29 09:02:34 -0500

    acconting for lower DPI setting by scaling the laptop screen

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 629x1440 --rotate normal --output DP-1 --mode 3440x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
+xrandr \
+    --output eDP-1 --primary --mode 1920x1080 --pos 629x1440 --rotate normal --scale 1x1 \
+    --output DP-1 --mode 3440x1440 --pos 0x0 --rotate normal \
+    --output HDMI-1 --off \
+    --output DP-2 --off \
+    --output HDMI-2 --off
+xrandr \
+    --output eDP-1 --primary --mode 1920x1080 --pos 629x1440 --rotate normal --scale .75x.75 \
+    --output DP-1 --mode 3440x1440 --pos 0x0 --rotate normal \
+    --output HDMI-1 --off \
+    --output DP-2 --off \
+    --output HDMI-2 --off
+
+
+
+#    --output eDP-1 --primary --mode 1920x1080 --pos 629x1440 --rotate normal --scale 1.5x1.5\
