3acc7987b3c713b50e8f5b4cfc4ffe8601c8f223
Rueben Ramirez <ruebenramirez@gmail.com>
2021-11-12 13:18:49 -0600

    update volume scripts

--- bin/volume-down.sh
+++ bin/volume-down.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# if pulseaudio daemon running: set volume info through it
-if [ -f /etc/pulse/daemon.conf ]; then
-    amixer -D pulse sset Master 5%-
-else
-    amixer sset Master 5%-
-fi;
+set -e +x
+
+# # if pulseaudio daemon running: set volume info through it
+# if [ -f /etc/pulse/daemon.conf ]; then
+#     amixer -D pulse sset Master 5%-
+# else
+#     amixer sset Master 5%-
+# fi;
+
+amixer -D pulse sset Master 5%-
+amixer sset Master 5%-
