33e9e7c587cb80f47f3f5b0476e8a05c0ae725a6
Rueben Ramirez <ruebenramirez@gmail.com>
2021-12-02 08:06:18 -0600

    updating display configuration XPS 17 + 4k monitor
    - optimal screen config for XPS 17 with 4k external monitor positioned
      above

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,23 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 xrandr --auto
 
-# I don't know why, but x11 requires resetting the scale to 1x1 before changing to another value.
-# If I don't do this, I end up missing half of my display on by laptops internal display.
-
-xrandr \
-    --output eDP-1 --primary --mode 1920x1080 --pos 1060x1728 --rotate normal --scale 1x1 \
-    --output DP-1 --mode 3440x1440 --pos 0x0 --rotate normal --scale 1x1\
-    --output HDMI-1 --off \
-    --output DP-2 --off \
-    --output HDMI-2 --off
-
-xrandr \
-    --output eDP-1 --primary --mode 1920x1080 --pos 1060x1728 --rotate normal  \
-    --output DP-1 --mode 3440x1440 --pos 0x0 --rotate normal --scale 1.2x1.2 \
-    --output HDMI-1 --off \
-    --output DP-2 --off \
-    --output HDMI-2 --off
-
-
-#    --output eDP-1 --primary --mode 1920x1080 --pos 629x1440 --rotate normal --scale 1.5x1.5\
+xrandr --output eDP-1 --primary --mode 3840x2400 --pos 960x3240 --rotate normal --scale 1x1 \
+    --output DP-1 --off --output DP-2 --off \
+    --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal --scale 1.5x1.5 \
+    --output DP-4 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off
