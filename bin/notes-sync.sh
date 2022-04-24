17c3f0c8a2f637fdc4f4ec16015c5898c4c8c93c
Rueben Ramirez <ruebenramirez@gmail.com>
2022-04-24 17:18:53 -0500

    make notes-sync script sync more nicely

--- bin/notes-sync.sh
+++ bin/notes-sync.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # leverage ssh-agent session without requiring interactive passphrase entry (pull passphrase from keychain)
 eval `keychain --noask --eval ~/.ssh/id_rsa`
@@ -9,7 +9,7 @@ cd /home/rramirez/Documents/notes/
 git pull --no-rebase
 
 NOTES_UPDATED="$(git status --porcelain | wc -l)"
-if [ "$NOTES_UPDATED" -gt 0 ]; then
+if [[ "$NOTES_UPDATED" -gt 0 ]]; then
     git add .
     git commit -q -m "Last Sync: $(date +"%Y-%m-%d %H:%M:%S") cron running from: $(hostname -f)"
 fi
