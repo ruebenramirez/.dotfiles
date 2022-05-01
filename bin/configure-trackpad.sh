bc0f5fa23c51b26d8227fbffef8990706b988c3e
Rueben Ramirez <ruebenramirez@gmail.com>
2022-05-01 17:22:40 -0500

    reconfiguring trackball

--- bin/configure-trackpad.sh
+++ bin/configure-trackpad.sh
@@ -2,20 +2,10 @@
 
 set -e +x
 
-TOUCHPAD=$(~/bin/touchpad-name)
-
-
 echo "STARTING trackball setup"
-
-# xinput --list | grep -i ergo m575
-# ERGO M575 Mouse                           id=25   [slave  pointer  (2)]
-
-
 TRACKBALL_DEVICE=$(xinput --list | grep "MX Ergo" | cut -d '=' -f 2 | cut -f 1)
 if [[ $(xinput list | grep "$TRACKBALL_DEVICE" | wc -l) > 0 ]]; then
     echo "trackball found: Logitech MX Ergo Trackball"
-
-
 #   [~]$ xinput list-props 17
 #Device 'Logitech MX Ergo Multi-Device Trackball ':
 #        Device Enabled (187):   1
@@ -48,14 +38,14 @@ if [[ $(xinput list | grep "$TRACKBALL_DEVICE" | wc -l) > 0 ]]; then
 #        libinput Drag Lock Buttons (338):       <no items>
 #        libinput Horizontal Scroll Enabled (339):       1
 
-
+    xinput set-prop "$TRACKBALL_DEVICE" "libinput Accel Speed" .85
     xinput set-prop "$TRACKBALL_DEVICE" "libinput Scroll Method Enabled" 0, 0, 1
     xinput set-prop "$TRACKBALL_DEVICE" "libinput Button Scrolling Button Lock Enabled" 1
 fi;
+echo "FINISHED trackball setup"
 
 
-echo "STARTING trackpad setup"
-
+echo "STARTING apple trackpad setup"
 # configure trackpad
 APPLE_TRACKPAD="Apple Inc. Magic Trackpad 2"
 if [[ $(xinput list | grep "$APPLE_TRACKPAD" | wc -l) > 0 ]]; then
@@ -63,42 +53,38 @@ if [[ $(xinput list | grep "$APPLE_TRACKPAD" | wc -l) > 0 ]]; then
     xinput set-prop "$APPLE_TRACKPAD" "libinput Tapping Enabled" 0
     xinput set-prop "$APPLE_TRACKPAD" "libinput Natural Scrolling Enabled" 1
     xinput set-prop "$APPLE_TRACKPAD" "libinput Click Method Enabled" {0 1}
-
     xinput set-prop "$APPLE_TRACKPAD" "libinput Accel Speed" .4
+    xinput set-prop "$APPLE_TRACKPAD" "libinput Accel Profile Enabled" 1, 0
+
     if [[ $(~/bin/is-xps-17) -gt 0 ]]; then
         echo "speeding up cursor accel for XPS 17"
         xinput set-prop "$APPLE_TRACKPAD" "libinput Accel Speed" .85
     fi
-    xinput set-prop "$APPLE_TRACKPAD" "libinput Accel Profile Enabled" 1, 0
 
     xinput --enable "$APPLE_TRACKPAD"
     xinput list-props "$APPLE_TRACKPAD"
 fi;
+echo "FINISHED apple trackpad setup"
+
+echo "STARTING trackpad setup"
+
+TOUCHPAD=$(~/bin/touchpad-name)
+TOUCHPADS_FOUND=$(echo $TOUCHPAD | wc -l)
 
 # configure trackpad
-if [[ $(~/bin/touchpad-name | wc -l) -gt 0 ]]; then
+if [[ $TOUCHPADS_FOUND -gt 0 ]]; then
     echo "touchpad found: $TOUCHPAD"
-
-    # disable tap to click
     xinput set-prop "$TOUCHPAD" "libinput Tapping Enabled" 0
-
-    # enable natural scrolling
     xinput set-prop "$TOUCHPAD" "libinput Natural Scrolling Enabled" 1
-
-    # set click method
     xinput set-prop "$TOUCHPAD" "libinput Click Method Enabled" {0 1}
-
-    # set accel speed
     xinput set-prop "$TOUCHPAD" "libinput Accel Speed" .3
+    xinput set-prop "$TOUCHPAD" "libinput Accel Profile Enabled" 1, 0
+    xinput --enable "$TOUCHPAD"
 
     if [[ $(~/bin/is-xps-17) -gt 0 ]]; then
-        #echo "speeding up cursor accel for XPS 17"
+        echo "speeding up cursor accel for XPS 17"
         xinput set-prop "$TOUCHPAD" "libinput Accel Speed" .85
     fi
-    xinput set-prop "$TOUCHPAD" "libinput Accel Profile Enabled" 1, 0
-
-    # enable touchpad
-    xinput --enable "$TOUCHPAD"
 
     #xinput list-props "$TOUCHPAD"
 fi
