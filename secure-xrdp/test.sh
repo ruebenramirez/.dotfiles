59debe72b337dddd686955cfe0d1a5a72d6711fe
Rueben Ramirez <ruebenramirez@gmail.com>
2022-08-07 20:56:16 -0500

    cleanup some syntax in the xrdp test

--- secure-xrdp/test.sh
+++ secure-xrdp/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PORT_OPEN=$(nmap 104.225.219.30 -p 3389|grep open | wc -l)
+PORT_OPEN=$(nmap 104.225.219.30 -p 3389 | grep open | wc -l)
 
 if [[ $PORT_OPEN -gt 0 ]]; then
     echo "port is open to the public internet"
