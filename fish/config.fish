537bb86f02be3ee53851c26641fbaa36a7721456
Rueben Ramirez <ruebenramirez@gmail.com>
2023-11-30 01:27:56 -0600

    cleaning up dockre image cleanup cmd

--- fish/config.fish
+++ fish/config.fish
@@ -84,7 +84,7 @@ alias d="docker"
 alias dps="docker ps -a"
 alias di="docker images"
 alias dat="docker attach --sig-proxy=true"
-alias dci="docker images | grep none | awk '{print $3}' | xargs sudo docker rmi -f"
+alias dci="docker images | grep none | awk '{print $3}' | xargs -I'{}' sudo docker rmi -f {}"
 alias dc="docker rm $(docker ps -q -a)"
 alias dcs="docker-compose"
 
