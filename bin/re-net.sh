f4e484c9d88eb0a056d0ee0df30db3f96e7cb8a0
Rueben Ramirez <ruebenramirez@gmail.com>
2023-12-01 08:44:30 -0600

    rename network reset script

--- bin/re-net.sh
+++ bin/re-net.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "restarting wifi connection"
+sudo systemctl stop wpa_supplicant.service
+sudo systemctl start wpa_supplicant.service
 
 echo "restarting tailscale connection"
 sudo systemctl restart tailscaled.service
