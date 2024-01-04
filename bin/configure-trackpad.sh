6fe4cae8fe56365f34fc4dbee2b45986dbe1c5ec
Rueben Ramirez <ruebenramirez@gmail.com>
2024-01-04 12:32:50 -0600

    disable tap to click on trackpad

--- bin/configure-trackpad.sh
+++ bin/configure-trackpad.sh
@@ -6,8 +6,8 @@ echo "STARTING trackpad setup"
 TRACKPAD="$(~/bin/trackpad-name)"
 if [[ $(xinput list | grep "$TRACKPAD" | wc -l) > 0 ]]; then
     echo "trackpad found: $TRACKPAD"
-    #xinput set-prop "$TRACKPAD" "libinput Tapping Enabled" 0
-    xinput set-prop "$TRACKPAD" "libinput Tapping Enabled" 1
+    xinput set-prop "$TRACKPAD" "libinput Tapping Enabled" 0
+    #xinput set-prop "$TRACKPAD" "libinput Tapping Enabled" 1
     xinput set-prop "$TRACKPAD" "libinput Natural Scrolling Enabled" 1
     xinput set-prop "$TRACKPAD" "libinput Click Method Enabled" {0 1}
     xinput set-prop "$TRACKPAD" "libinput Accel Speed" .85
