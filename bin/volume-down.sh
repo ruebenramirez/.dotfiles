9253ab6686e1c27a4139b9f9b1ed2bb16d64e7f4
Rueben Ramirez <ruebenramirez@gmail.com>
2024-03-24 09:02:37 -0500

    update volume change scripts for pipewire compatibility

--- bin/volume-down.sh
+++ bin/volume-down.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-set -e +x
-
-# # if pulseaudio daemon running: set volume info through it
-# if [ -f /etc/pulse/daemon.conf ]; then
-#     amixer -D pulse sset Master 5%-
-# else
-#     amixer sset Master 5%-
-# fi;
-
-# amixer -D pulse sset Master 5%-
-# amixer sset Master 5%-
-
-pactl set-sink-volume @DEFAULT_SINK@ -10%
-
+amixer sset Master 5%-
--- bin/volume-get.sh
+++ bin/volume-get.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-VOL=""
-
-# if pulseaudio daemon running: get volume info from it
-if [ -f /etc/pulse/daemon.conf ]; then
-    VOL="$(amixer -D pulse get Master | tail -1 | sed 's/.*\[\([0-9]*%\)\].*/\1/')"
-else
-    VOL="$(amixer get Master | tail -1 | sed 's/.*\[\([0-9]*%\)\].*/\1/')"
-fi;
+VOL="$(amixer get Master | tail -1 | sed 's/.*\[\([0-9]*%\)\].*/\1/')"
 
 echo "vol. $VOL"
