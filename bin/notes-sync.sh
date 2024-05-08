85528b12a44093f6595d77e1c5da39109282a5e8
Rueben Ramirez <ruebenramirez@gmail.com>
2024-05-08 09:35:27 -0500

    sync notes to/from git repo

--- bin/notes-sync.sh
+++ bin/notes-sync.sh
@@ -6,12 +6,21 @@
 cd /home/rramirez/Documents/notes/
 
 #git fetch --all
-git pull --no-rebase
+#git pull -r origin master
+git pull --no-rebase origin master
+echo "git pull complete"
 
 NOTES_UPDATED="$(git status --porcelain | wc -l)"
 if [[ "$NOTES_UPDATED" -gt 0 ]]; then
     git add .
-    git commit -q -m "Last Sync: $(date +"%Y-%m-%d %H:%M:%S") cron running from: $(hostname -f)"
+    msg="Last Sync: $(date +"%Y-%m-%d %H:%M:%S") cron running from: $(hostname)"
+    git commit -q -m "$msg"
+    echo "$msg"
+
+    git push -v origin master
+    echo "git push of local changes complete"
+else
+    echo "no changes to commit/push"
 fi
 
-git push -q
+
