7aab286188897d4df886b30a3e1d170c0b062e95
Rueben Ramirez <ruebenramirez@gmail.com>
2024-05-09 13:13:11 -0500

    notes sync updates to improve reliability on various machines

--- bin/notes-sync.sh
+++ bin/notes-sync.sh
@@ -2,23 +2,33 @@
 
 set -ex
 
-cd /home/rramirez/Documents/notes/
+echo 'dynamically source $NOTES_DIR from $NOTES_ENV_FILE'
+# This is necessary because my notes are checked out into
+#   different directories on different machines:
+#   - NixOS: ~/Documents/notes,
+#   - WSL2: /mnt/d/Documents/notes/
+#   - Termux: ~/storage/shared/notes-ssh/
+NOTES_ENV_FILE=~/.notes-env
+if [[ -z "$NOTES_ENV_FILE" ]]; then
+    echo "We're missing a $NOTES_ENV_FILE file"
+    exit 1
+fi
+source $NOTES_ENV_FILE
+cd $NOTES_DIR
 
 #git fetch --all
 #git pull -r origin master
 git pull --no-rebase origin master
 echo "git pull complete"
 
+# sync local working copy changes to the remote repo
 NOTES_UPDATED="$(git status --porcelain | wc -l)"
 if [[ "$NOTES_UPDATED" -gt 0 ]]; then
+    echo "local working copy changes being synced to remote repo"
+    GIT_COMMIT_MSG="Last Sync: $(date +"%Y-%m-%d %H:%M:%S") cron running from: $(hostname -f)"
     git add .
-    msg="Last Sync: $(date +"%Y-%m-%d %H:%M:%S") cron running from: $(hostname)"
-    git commit -q -m "$msg"
-    echo "$msg"
-
-    echo "git push of local changes complete"
+    git commit -q -m "$GIT_COMMIT_MSG"
 fi
 
-git push -v origin master
-
-
+git push origin/master
+echo "local notes synced to repo remote"
