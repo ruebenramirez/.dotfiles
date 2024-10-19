3112687a7f0cb0e3da7913be83e3a7086d447948
Rueben Ramirez <ruebenramirez@gmail.com>
2024-10-19 08:27:46 -0500

    toggle trackpad for wayland/sway env

--- bin/trackpad-toggle.sh
+++ bin/trackpad-toggle.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-TRACKPAD="$(~/bin/trackpad-name)"
-if [[ $(xinput --list "$TRACKPAD" | grep -ic disabled) > 0 ]]; then
-    xinput --enable "$TRACKPAD"
-else
-    xinput --disable "$TRACKPAD"
-fi;
-
+swaymsg input type:touchpad events toggle
