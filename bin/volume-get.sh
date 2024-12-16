6306e4104fad53190aad0ac90882b5dd9e2b4225
Rueben Ramirez <ruebenramirez@gmail.com>
2024-12-15 19:46:56 -0600

    print "MUTED" in sway bar if volume muted

--- bin/volume-get.sh
+++ bin/volume-get.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+if [[ $(pactl get-sink-mute $(pactl get-default-sink) | grep -ic 'yes')  -gt 0 ]]; then
+    echo "MUTED"
+    exit 1
+fi
+
 pactl get-sink-volume $(pactl get-default-sink) | awk '{print $5}'
