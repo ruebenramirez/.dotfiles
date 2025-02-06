4cd21ad5e3aa4cef14978457661692aa777e9414
Rueben Ramirez <ruebenramirez@gmail.com>
2025-02-06 07:18:25 -0600

    revert nerdctl fish aliases back to docker

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -106,13 +106,13 @@ alias jjds="jj describe"
 alias jja="jj abandon"
 
 # docker aliases
-alias d="nerdctl"
-alias dps="nerdctl ps -a"
-alias di="nerdctl images"
-alias dat="nerdctl attach --sig-proxy=true"
-alias dci="nerdctl images | grep none | awk '{print $3}' | xargs -I'{}' sudo nerdctl rmi -f {}"
-alias dc="nerdctl rm $(nerdctl ps -q -a)"
-alias dcs="nerdctl-compose"
+alias d="docker"
+alias dps="docker ps -a"
+alias di="docker images"
+alias dat="docker attach --sig-proxy=true"
+alias dci="docker images | grep none | awk '{print $3}' | xargs -I'{}' sudo docker rmi -f {}"
+alias dc="docker rm $(docker ps -q -a)"
+alias dcs="docker-compose"
 
 # terraform aliases
 alias tf="terraform"
