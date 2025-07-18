4e617fbc30d920104f7d25a6915047bca49ca39d
Rueben Ramirez <ruebenramirez@gmail.com>
2025-07-18 03:59:46 -0500

    max volume 100%

--- bin/volume-up.sh
+++ bin/volume-up.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-pactl set-sink-volume @DEFAULT_SINK@ +5%
+VOLUME=$(pactl get-sink-volume $(pactl get-default-sink) | awk '{print $5}' | cut -d '%' -f 1)
+
+if [[ $VOLUME -lt 100 ]]; then
+    pactl set-sink-volume @DEFAULT_SINK@ +5%
+fi
